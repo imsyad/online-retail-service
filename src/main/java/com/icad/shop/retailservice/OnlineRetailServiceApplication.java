d1e3bdbf52e88480f08cc5b51e29e0a0c577ffc6
Irsyad Muhammad <irsyadmuhammad.020201@gmail.com>
2024-06-04 14:00:12 +0700

    feat: intialize spring boot project

---
+++ src/main/java/com/icad/shop/retailservice/OnlineRetailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.icad.shop.retailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRetailServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/icad/shop/retailservice/OnlineRetailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icad.shop.retailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRetailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
